02d1e261c7edf7a7ecc68b63170c151705d21b2b
Shane Arthur <shane_j_arthur@homedepot.com>
2019-02-12 14:10:50 -0500

    Add in dummy config file.

---
+++ config/secrets.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    secrets: {
+        ACCESS_KEY_ID : 'AKIAIFFUMNVKXFTGATLQ',
+        ACCESS_SECRET_ID : 'DbL6zmVf8GAODMbnNTn9euSmHj9C1OPAbINu9FTo'
+    },
+
+    indexes: {
+        ACTIVITY: 'campinfo-activities',
+        CAMPS: 'campinfo-camps',
+        CAMP_ACTIVITY: 'campinfo-camp_activities'
+    }
+};
