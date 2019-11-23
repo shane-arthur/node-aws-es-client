7a31840dec4f8e15f695ba92f8f164b7384c4ba1
Shane Arthur <shane.arthur@gmail.com>
2019-11-23 13:26:01 -0500

    Remove secrets from remote.

---
+++ config/secrets.config.js
@@ -0,0 +1,13 @@
+
+
+export const SECRETS = {
+        ACCESS_KEY_ID : 'AKIAIFFUMNVKXFTGATLQ',
+        ACCESS_SECRET_ID : 'DbL6zmVf8GAODMbnNTn9euSmHj9C1OPAbINu9FTo'
+    };
+
+   export const INDEXES = {
+        ACTIVITY: 'campinfo-activities',
+        CAMPS: 'campinfo-camps',
+        CAMP_ACTIVITY: 'campinfo-camp_activities'
+    };
+    
